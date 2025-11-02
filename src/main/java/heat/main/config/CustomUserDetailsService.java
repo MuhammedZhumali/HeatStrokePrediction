fdecd0c456f73e4d2e0278fce630ef0cb28d4af7
stnqq <life33890@gmail.com>
2025-11-03 03:58:21 +0500

    unused imports

--- src/main/java/heat/main/config/CustomUserDetailsService.java
+++ src/main/java/heat/main/config/CustomUserDetailsService.java
@@ -2,18 +2,10 @@ package heat.main.config;
 
 import heat.main.domain.User;
 import heat.main.users.repository.UserRepository;
-import heat.main.users.service.UserSerivce;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
